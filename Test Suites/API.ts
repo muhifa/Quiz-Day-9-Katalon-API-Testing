9818b8075e82c2af6778e78ce6d6c0aec94f0cda
Muhifa <Muhifa@iiYzy>
2023-01-25 21:39:24 +0700

    menambahakan Booking TC

---
+++ Test Suites/API.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e2b454fb-1254-426c-bf82-ef9340fd9fe3</testSuiteGuid>
+</TestSuiteEntity>
